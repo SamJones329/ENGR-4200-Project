e553ff0bff539822fea40e715fc666998d6f5411
Samuel Jones <spjones329@gmail.com>
2022-11-02 14:55:01 -0500

    particle filter progress.

--- dory_localization/src/particle_filter.cpp
+++ dory_localization/src/particle_filter.cpp
@@ -23,8 +23,8 @@ namespace DoryLoc {
         double mapYmin;
         double mapYmax;
         VectorXd pWei; 
-        VectorXd pAng; 
-        MatrixXd pxy;
+        VectorXd pYaw; 
+        MatrixXd pxyz;
 
         // Gaussian Constants
         double valRng;
@@ -33,7 +33,7 @@ namespace DoryLoc {
         double angSigSq2;
 
         void setParticleAngles(VectorXd newAngles) {
-            this->pAng = newAngles;
+            this->pYaw = newAngles;
             //TODO - wrap angles
         }
 
@@ -58,9 +58,9 @@ namespace DoryLoc {
             VectorXd wei(num);
             this->pWei = wei;
             VectorXd ang(num);
-            this->pAng = ang;
-            MatrixXd xy = ArrayXXd::Zero(2,num);
-            this->pxy = xy;
+            this->pYaw = ang;
+            MatrixXd xyz = ArrayXXd::Zero(3,num);
+            this->pxyz = xyz;
 
         }
 
@@ -81,14 +81,14 @@ namespace DoryLoc {
             for(int i = 0; i < this->num; i++) {
                 odomAngVec << odom[2];
             }
-            setParticleAngles(this->pAng + odomAngVec);
+            setParticleAngles(this->pYaw + odomAngVec);
 
             Vector2d odomTranslation {odom[0], odom[1]};
 
 
             // try resizing to vector of matrices so can use vector iterator
-            VectorXd angCos = this->pAng.array().cos();
-            VectorXd angSin = this->pAng.array().sin();
+            VectorXd angCos = this->pYaw.array().cos();
+            VectorXd angSin = this->pYaw.array().sin();
             Matrix<VectorXd, 2, 2> R;// {angCos, -angSin};//,{angSin, angCos}};
             R(0,0) = angCos;
             R(0,1) = -angSin;
@@ -103,14 +103,28 @@ namespace DoryLoc {
                 deltaWc.row(1) << R(1,0)(i) * odomTranslation(0) + R(1,1)(i) * odomTranslation(1);
             }
 
-            this->pxy += deltaWc;
+            this->pxyz += deltaWc;
 
             // update flag for resampling
             this->moving = true;
         }
 
-        void weight() {
-
+        void weight(double* odom) {
+            VectorXd weights = ArrayXd::Zero(this->num);
+            
+            
+            for(int i = 0; i < this->num; i++) {
+                double x = pxyz(0,i);
+                double y = pxyz(1,i);
+                double z = pxyz(2,i);
+                double wx = 1 / ( measRngNoise * sqrt(2*M_PI) * exp(-pow(x-odom[0], 2) / (2*pow(measRngNoise, 2))) );
+                double wy = 1 / ( measRngNoise * sqrt(2*M_PI) * exp(-pow(y-odom[1], 2) / (2*pow(measRngNoise, 2))) );
+                double wz = 1 / ( measRngNoise * sqrt(2*M_PI) * exp(-pow(z-odom[2], 2) / (2*pow(measRngNoise, 2))) );
+                weights(i) = wx + wy + wz;
+            }
+            double wsum = weights.sum();
+            weights /= wsum;
+            this->pWei = weights;
         }
 
         void resample() {
